ea4a84572c03a02dd5e65846087214cea0b59532
Andrei Gorshkov <andruhinsans@mail.ru>
2024-07-19 21:06:02 +0300

    homework after test 1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
         if (clientOS == 0){
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1)
-        System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
         System.out.println();
 
@@ -18,8 +19,9 @@ public class Main {
 
         if (clientOS == 0 && clientDeviceYear<2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear<2015)
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
 
         System.out.println();
 
@@ -55,46 +57,48 @@ public class Main {
 
         //task5 сезон года
         System.out.println("task5");
-        int monthNumber = 4;
-        switch (monthNumber) {
-            case 1:
-                System.out.println("1-й месяц (он же январь) принадлежит к сезону зима");
-                break;
-            case 2:
-                System.out.println("2-й месяц (он же февраль) принадлежит к сезону зима");
-                break;
-            case 3:
-                System.out.println("3-й месяц (он же март) принадлежит к сезону весна" );
-                break;
-            case 4:
-                System.out.println("4-й месяц (он же апрель) принадлежит к сезону весна");
-                break;
-            case 5:
-                System.out.println("5-й месяц (он же май) принадлежит к сезону весна");
-                break;
-            case 6:
-                System.out.println("6-й месяц (он же июнь) принадлежит к сезону лето");
-                break;
-            case 7:
-                System.out.println("7-й месяц (он же июль) принадлежит к сезону лето");
-                break;
-            case 8:
-                System.out.println("8-й месяц (он же август) принадлежит к сезону лето");
-                break;
-            case 9:
-                System.out.println("9-й месяц (он же сентябрь) принадлежит к сезону осень");
-                break;
-            case 10:
-                System.out.println("10-й месяц (он же октябрь) принадлежит к сезону осень");
-                break;
-            case 11:
-                System.out.println("11-й месяц (он же ноябрь) принадлежит к сезону осень");
-                break;
-            case 12:
-                System.out.println("12-й месяц (он же декабрь) принадлежит к сезону зима");
-                break;
-            default:
-                System.out.println();  //Пропишите условие, при котором программа не будет выполняться
+        int monthNumber = 3;
+        if (monthNumber<=12) {
+            switch (monthNumber) {
+                case 1:
+                    System.out.println("1-й месяц (он же январь) принадлежит к сезону зима");
+                    break;
+                case 2:
+                    System.out.println("2-й месяц (он же февраль) принадлежит к сезону зима");
+                    break;
+                case 3:
+                    System.out.println("3-й месяц (он же март) принадлежит к сезону весна");
+                    break;
+                case 4:
+                    System.out.println("4-й месяц (он же апрель) принадлежит к сезону весна");
+                    break;
+                case 5:
+                    System.out.println("5-й месяц (он же май) принадлежит к сезону весна");
+                    break;
+                case 6:
+                    System.out.println("6-й месяц (он же июнь) принадлежит к сезону лето");
+                    break;
+                case 7:
+                    System.out.println("7-й месяц (он же июль) принадлежит к сезону лето");
+                    break;
+                case 8:
+                    System.out.println("8-й месяц (он же август) принадлежит к сезону лето");
+                    break;
+                case 9:
+                    System.out.println("9-й месяц (он же сентябрь) принадлежит к сезону осень");
+                    break;
+                case 10:
+                    System.out.println("10-й месяц (он же октябрь) принадлежит к сезону осень");
+                    break;
+                case 11:
+                    System.out.println("11-й месяц (он же ноябрь) принадлежит к сезону осень");
+                    break;
+                case 12:
+                    System.out.println("12-й месяц (он же декабрь) принадлежит к сезону зима");
+                    break;
+                default:
+                    System.out.println("Нет такого месяца");
+            }
         }
     }
 }
